a75253647bf50ba941f2f0a49fcfd49af36ec4db
Kysss <yxia@oswego.edu>
2018-05-03 21:32:41 -0400

    updates on packet arguments and updates on updateManager

--- MYB-Chain/src/common/Addresses.java
+++ MYB-Chain/src/common/Addresses.java
@@ -4,10 +4,10 @@ package common;
  * Created by Michael on 5/2/2018.
  */
 public class Addresses {
-    public static String USER_REQUEST_ADDRESS;
-    public static String USER_RECEIVE_UPDATE_ADDRESS;
-    public static String MINER_PROPOSAL_ADDRESS;
-    public static String MINER_LEARNING_ADDRESS;
-    public static String CHECKER_ACCEPTANCE_ADDRESS;
-    public static String CHECKER_LEARNING_ADDRESS;
+    public static final String USER_REQUEST_ADDRESS;
+    public static final String USER_RECEIVE_UPDATE_ADDRESS;
+    public static final String MINER_PROPOSAL_ADDRESS;
+    public static final String CHECKER_ACCEPTANCE_ADDRESS;
+    public static final String MINER_LEARNING_ADDRESS;
+    public static final String HOLDER_LEARNING_ADDRESS;
 }
