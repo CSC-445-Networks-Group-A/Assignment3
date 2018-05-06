6f5145e0e51654208a61ef77fd85d43e0190c5d1
Brian Dorsey <bdo528@gmail.com>
2018-05-05 22:55:35 -0400

    hash generation typo fixed

--- MYB-Chain/src/client/Client.java
+++ MYB-Chain/src/client/Client.java
@@ -2,25 +2,15 @@ package client;
 
 import chain.User;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 public class Client {
-    private static User myUser;
 
     public static void main(String args[]){
 
-//        User myUser;
         if(User.userFileExists()){
-            myUser = User.loadUser();
+            User myUser = User.loadUser();
+//            myUser.updateBlockChain();
             ClientUI client = new ClientUI(myUser);
             client.setVisible(true);
             client.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
